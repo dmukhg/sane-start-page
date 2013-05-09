b907a71476a8410e050bdc5bcd0d40abfb45e594
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-05-09 23:33:11 +0530

    Add preliminary clock and bootstrap css layout

---
+++ javascript/clock.js
@@ -0,0 +1,46 @@
+// Copyright (c) 2013, Dipanjan Mukherjee
+
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December']
+
+  , DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+    'Saturday'];
+
+
+var zeroPad = function (number) {
+  // Add a zero before the number if less than 10
+  return number < 10 ? "0" + number : number;
+};
+
+var setupTime = function (selector) {
+  var today = new Date()
+    , h = today.getHours()
+    , m = today.getMinutes();
+
+  // add a zero in front of numbers<10
+  m = zeroPad(m);
+  h = zeroPad(h);
+
+  document.getElementById(selector).innerHTML = h + ":" + m;
+
+  // Call this function every half minute
+  t = setTimeout(function () { setupTime(selector); },3000);
+};
+
+var setupDate = function (selector) {
+  var today = new Date()
+    , w = today.getDay()
+    , d = today.getDate()
+    , m = today.getMonth();
+
+  // Add a zero to date
+  d = zeroPad(d);
+
+  document.getElementById(selector).innerHTML = DAYS[w] + ", " + d + " " + MONTHS[m];
+};
+
+// Initialization
+document.addEventListener('DOMContentLoaded', function () {
+  setupTime('time');
+  setupDate('date');
+});
