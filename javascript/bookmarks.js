9135ae561060e6d6abde7e250a18e38d097854df
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-05-10 01:38:41 +0530

    Add bookmarks view; ATM only show tree hierarchy, no editing

---
+++ javascript/bookmarks.js
@@ -0,0 +1,83 @@
+// Copyright (c) 2013, Dipanjan Mukherjee
+
+var NodeView = function (bookmarkTreeNode) {
+  // Parses a single item into an li view
+  var li = document.createElement('li')
+    , a  = document.createElement('a');
+
+  if (bookmarkTreeNode.url === undefined) {
+    // Folder node
+    a.href = '#id'
+    li.className = 'folder';
+ }
+
+  a.innerHTML = bookmarkTreeNode.title;
+
+  li.appendChild(a);
+  return li;
+};
+
+var TreeView = function (node) {
+  // Parses the entire tree into a ul > li view
+  var ul, li, item = {};
+
+  ul = document.createElement('ul');
+
+  if (node.hasOwnProperty('parentId')) {
+    item['parentId'] = node['parentId'];
+  }
+
+  item['id']    = node['id'];
+  item['title'] = node['title'];
+  item['url']   = node['url'];
+
+  // If id is 0, this is the root of all bookmarks
+  if (item.id === '0') {
+    item.title = 'All bookmarks'
+  }
+
+  li = NodeView(item);
+  ul.appendChild(li);
+  
+  if (node.hasOwnProperty('children')) {
+    var i=0, children = node['children'], c_ul;
+
+    c_ul = document.createElement('ul');
+
+    for (; i<children.length; i++) {
+      var c_li = TreeView(children[i]);
+
+      c_ul.appendChild(c_li);
+    }
+
+    ul.appendChild(c_ul);
+
+    // Register tree toggling actions
+    li.onclick = function () {
+      if (c_ul.className === 'hidden') {
+        c_ul.className = '';
+      } else {
+        c_ul.className = 'hidden';
+      }
+    };
+  } else {
+    return li;
+  }
+
+  return ul;
+};
+
+var setupBookmarks = function (selector) {
+  var div = document.getElementById(selector);
+
+  chrome.bookmarks.getTree(function (tree) {
+    var view = TreeView(tree[0]);
+    console.log(tree);
+
+    div.appendChild(view);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  setupBookmarks('bookmarks-list');
+});
