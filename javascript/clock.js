f645cfa15d1a3088e0a8dd04cfdfdc19644b3111
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-05-12 01:26:10 +0530

    Semantic documentation and code explanation in clock.js

--- javascript/clock.js
+++ javascript/clock.js
@@ -8,13 +8,15 @@
   , DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
       'Saturday']
 
-
   , zeroPad = function (number) {
-    // Add a zero before the number if less than 10
+    /* Add a zero before the @param number if it is less than 10 less than 10
+     * @return {string} since that is what will be fed into the DOM. */
     return number < 10 ? "0" + number : number;
   }
 
   , setupTime = function (selector) {
+    /* Calculate current time and put that time as innerHTML into 
+     * @param selector.  Also setTimeout for repeating this every 30 seconds. */
     var today = new Date()
       , h = today.getHours()
       , m = today.getMinutes();
@@ -25,11 +27,15 @@
 
     document.getElementById(selector).innerHTML = h + ":" + m;
 
-    // Call this function every half minute
+    // Call this function every half minute, since time in hh:mm doesn't change
+    // all that frequently. :)
     t = setTimeout(function () { setupTime(selector); },3000);
   }
 
   , setupDate = function (selector) {
+    /* Calculate current date and put that date as innerHTML into 
+     * @param selector. This is a very optimistic function since there is no
+     * setTimeout call here. */
     var today = new Date()
       , w = today.getDay()
       , d = today.getDate()
