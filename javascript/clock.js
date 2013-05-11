cbaf4b436a4a742c8145200188870fc43d45a1c6
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-05-12 01:15:38 +0530

    Cleanup code and avoid GLOBAL VARS!!! in clock.js

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,46 +1,49 @@
 // Copyright (c) 2013, Dipanjan Mukherjee
 
-var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-    'August', 'September', 'October', 'November', 'December']
+// Initialization code is at the bottom
+;(function (document, Date) {
+  var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December']
 
   , DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
-    'Saturday'];
+      'Saturday']
 
 
-var zeroPad = function (number) {
-  // Add a zero before the number if less than 10
-  return number < 10 ? "0" + number : number;
-};
+  , zeroPad = function (number) {
+    // Add a zero before the number if less than 10
+    return number < 10 ? "0" + number : number;
+  }
 
-var setupTime = function (selector) {
-  var today = new Date()
-    , h = today.getHours()
-    , m = today.getMinutes();
+  , setupTime = function (selector) {
+    var today = new Date()
+      , h = today.getHours()
+      , m = today.getMinutes();
 
-  // add a zero in front of numbers<10
-  m = zeroPad(m);
-  h = zeroPad(h);
+    // add a zero in front of numbers<10
+    m = zeroPad(m);
+    h = zeroPad(h);
 
-  document.getElementById(selector).innerHTML = h + ":" + m;
+    document.getElementById(selector).innerHTML = h + ":" + m;
 
-  // Call this function every half minute
-  t = setTimeout(function () { setupTime(selector); },3000);
-};
+    // Call this function every half minute
+    t = setTimeout(function () { setupTime(selector); },3000);
+  }
 
-var setupDate = function (selector) {
-  var today = new Date()
-    , w = today.getDay()
-    , d = today.getDate()
-    , m = today.getMonth();
+  , setupDate = function (selector) {
+    var today = new Date()
+      , w = today.getDay()
+      , d = today.getDate()
+      , m = today.getMonth();
 
-  // Add a zero to date
-  d = zeroPad(d);
+    // Add a zero to date
+    d = zeroPad(d);
 
-  document.getElementById(selector).innerHTML = DAYS[w] + ", " + d + " " + MONTHS[m];
-};
+    document.getElementById(selector).innerHTML = DAYS[w] + ", " + d + " " + MONTHS[m];
+  };
 
-// Initialization
-document.addEventListener('DOMContentLoaded', function () {
-  setupTime('time');
-  setupDate('date');
-});
+  // Initialization
+  document.addEventListener('DOMContentLoaded', function () {
+    setupTime('time');
+    setupDate('date');
+  });
+})(document, Date);
